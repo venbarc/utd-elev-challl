6538a7153b2ef40c60d0621c6964db8e3bbe2b6a
venbarc <118934661+venbarc@users.noreply.github.com>
2026-03-09 10:28:03 +0800

    Implement elevator logic

--- elevator.js
+++ elevator.js
@@ -4,65 +4,132 @@ export default class Elevator {
     this.stops = 0
     this.floorsTraversed = 0
     this.requests = []
-    this.riders= []
+    this.riders = []
   }
 
-  dispatch(){
-    this.requests.forEach(request => {
-      if(this.riders.length || this.requests.length){
-        this.goToFloor(request)
+  dispatch() {
+    if (!this.requests.length && !this.riders.length) {
+      return
+    }
+
+    this.hasStop()
+
+    let direction = this.getInitialDirection()
+    if (!direction) {
+      if (this.checkReturnToLoby()) {
+        this.returnToLoby()
       }
-    })
+      return
+    }
+
+    while (this.requests.length || this.riders.length) {
+      if (direction === 'up') {
+        if (this.hasPendingAbove()) {
+          this.moveUp()
+        } else if (this.hasPendingBelow()) {
+          direction = 'down'
+        } else {
+          break
+        }
+      } else {
+        if (this.hasPendingBelow()) {
+          this.moveDown()
+        } else if (this.hasPendingAbove()) {
+          direction = 'up'
+        } else {
+          break
+        }
+      }
+    }
+
+    if (this.checkReturnToLoby()) {
+      this.returnToLoby()
+    }
   }
 
-  goToFloor(person){  
-    // add your code here
+  goToFloor(person) {
+    if (person && !this.requests.includes(person) && !this.riders.includes(person)) {
+      this.requests.push(person)
+    }
+    this.dispatch()
   }
 
-  moveUp(){
+  getInitialDirection() {
+    if (this.riders.length) {
+      return this.riders[0].dropOffFloor >= this.currentFloor ? 'up' : 'down'
+    }
+    if (this.requests.length) {
+      return this.requests[0].currentFloor >= this.currentFloor ? 'up' : 'down'
+    }
+    return null
+  }
+
+  hasPendingAbove() {
+    return this.requests.some(request => request.currentFloor > this.currentFloor) ||
+      this.riders.some(rider => rider.dropOffFloor > this.currentFloor)
+  }
+
+  hasPendingBelow() {
+    return this.requests.some(request => request.currentFloor < this.currentFloor) ||
+      this.riders.some(rider => rider.dropOffFloor < this.currentFloor)
+  }
+
+  moveUp() {
     this.currentFloor++
     this.floorsTraversed++
-    if(this.hasStop()){
+    if (this.hasStop()) {
       this.stops++
-    }    
+    }
   }
 
-  moveDown(){
-    if(this.currentFloor > 0){      
+  moveDown() {
+    if (this.currentFloor > 0) {
       this.currentFloor--
       this.floorsTraversed++
-      if(this.hasStop()){
+      if (this.hasStop()) {
         this.stops++
       }
     }
   }
 
-  hasStop(){
-    // add your code here
+  hasStop() {
+    const didPickup = this.hasPickup()
+    const didDropoff = this.hasDropoff()
+    return didPickup || didDropoff
   }
 
-  hasPickup(){
-    // add your code here
+  hasPickup() {
+    const pickups = this.requests.filter(request => request.currentFloor === this.currentFloor)
+    if (!pickups.length) {
+      return false
+    }
+
+    this.riders.push(...pickups)
+    this.requests = this.requests.filter(request => request.currentFloor !== this.currentFloor)
+    return true
   }
 
-  hasDropoff(){
-    // add your code here
+  hasDropoff() {
+    const startingCount = this.riders.length
+    this.riders = this.riders.filter(rider => rider.dropOffFloor !== this.currentFloor)
+    return this.riders.length !== startingCount
   }
 
-  checkReturnToLoby(){
-    // add your code here
+  checkReturnToLoby() {
+    return !this.riders.length && !this.requests.length && new Date().getHours() < 12
   }
 
-  returnToLoby(){
-    while(this.currentFloor > 0){
+  returnToLoby() {
+    while (this.currentFloor > 0) {
       this.moveDown()
     }
   }
 
-  reset(){
+  reset() {
     this.currentFloor = 0
     this.stops = 0
     this.floorsTraversed = 0
     this.riders = []
+    this.requests = []
   }
 }
